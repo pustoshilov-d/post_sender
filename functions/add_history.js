72891ca7165fa255ee14ce79f436734ad1c19d4e
Dmitry <DimkaPusha@gmail.com>
2020-04-19 23:17:21 +0300

    disable fix, broadcast history

--- functions/add_history.js
+++ functions/add_history.js
@@ -7,7 +7,7 @@ module.exports = async (group_id,post_link,to_chat_list,to_group, action_type) =
         const sql = `INSERT INTO history (post_link,to_group,to_chat_list, action_type) VALUES ('${post_link}', ${to_group}, ${to_chat_list}, '${action_type}')`;
         await client.query(sql);
         client.end();
-        console.log(group_id,'добавлено в историю', post_link,to_chat_list,to_group);
+        console.log(group_id,'добавлено в историю', post_link,to_chat_list,to_group, action_type);
     }
     catch (e) {
         console.log(group_id, 'ошибка в add_history.js', e)
--- functions/check_history.js
+++ functions/check_history.js
@@ -7,7 +7,7 @@ module.exports = async (group_id,post_link,to_chat_list,to_group, action_type) =
         const sql = `SELECT * FROM history WHERE post_link='${post_link}' AND to_group=${to_group} AND to_chat_list=${to_chat_list} AND action_type ='${action_type}'`;
         let res = await client.query(sql);
         client.end();
-        console.log(group_id,'проверена история', res.rowCount === 0, post_link,to_chat_list,to_group);
+        console.log(group_id,'проверена история', res.rowCount === 0, post_link,to_chat_list,to_group, action_type);
 
         return res.rowCount === 0;
     }
