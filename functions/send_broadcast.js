891687ef8515dbd48e50cc077bd1777670b1911a
Dmitry <DimkaPusha@gmail.com>
2020-04-21 19:43:32 +0300

    broadcast async fix

--- functions/send_broadcast.js
+++ functions/send_broadcast.js
@@ -23,8 +23,8 @@ module.exports = async (group_id, post_link, action) => {
                 })
             };
 
-            let error, response = await request(options);
-            console.log(error,response);
+            let error, req, response = await request(options);
+            console.log(error,req,response);
             if (!error){
                 await add_history(action.from_group,post_link,action.to_chat_list,action.to_group, action.action_type);
                 console.log(group_id, 'рассылка отправлена. Статус', JSON.parse(response.body).response.status, post_link);
