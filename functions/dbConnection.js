dd9a70a3de1df145a16beb97ea941ca7281092e0
NapITLab <Dimkach3>
2021-11-09 14:24:28 +0300

    test

--- functions/dbConnection.js
+++ functions/dbConnection.js
@@ -2,6 +2,8 @@ const {DATABASE_URL} = require('../config.js');
 const {Client} = require('pg');
 
 module.exports = function createClient () {
+  
+  console.log('База', DATABASE_URL);
   try{
     return new Client({
       connectionString: DATABASE_URL,
--- functions/get_actions.js
+++ functions/get_actions.js
@@ -3,11 +3,13 @@ const createClient = require('./dbConnection.js');
 module.exports = async (group_id, post_type) => {
     try{
         const client = await createClient();
-        await client.connect();
+        console.log('res client', client)
+        let res_connect = await client.connect();
+        console.log('res connect', res_connect)
         const sql = `SELECT * FROM actions WHERE disable_flag is NULL AND from_group = ${group_id} AND (post_type = '${post_type}' OR post_type = 'all')`;
         let res = await client.query(sql);
-        client.end();
         console.log(group_id,'Получены действия', res.rowCount);
+        client.end();
 
         return res.rows;
     }
